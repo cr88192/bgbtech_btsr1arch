18d2a942b9b93ab794fd88b547431ba867898381
cr88192 <cr88192@gmail.com>
2018-12-18 09:03:06 -0600

    Rename old core attempt to vlcore_old

--- jx2vlcore/obj_dir/VBsrExUnit__ALLcls.d
+++
@@ -1,6 +0,0 @@
-VBsrExUnit__ALLcls.o: VBsrExUnit__ALLcls.cpp VBsrExUnit.cpp VBsrExUnit.h \
- /usr/local/share/verilator/include/verilated_heavy.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilated_config.h \
- /usr/local/share/verilator/include/verilatedos.h VBsrExUnit__Syms.h \
- VBsrExUnit___024unit.h VBsrExUnit___024unit.cpp
--- jx2vlcore/obj_dir/sim_exunit0.d
+++
@@ -1,6 +0,0 @@
-sim_exunit0.o: ../sim_exunit0.cpp VBsrExUnit.h \
- /usr/local/share/verilator/include/verilated_heavy.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilated_config.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated.h
