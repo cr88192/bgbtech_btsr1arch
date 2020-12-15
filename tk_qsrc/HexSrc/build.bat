47f2677edea2f5c033c9683348b31c76a3891faf
cr88192 <cr88192@gmail.com>
2020-12-14 18:24:00 -0600

    Remove Hexen (filename case fix).

--- tk_qsrc/HexSrc/build.bat
+++
@@ -1,2 +0,0 @@
-make -C ../tk_clib -f Makefile.sh4bcc
-make -f Makefile.sh4bcc
--- tk_qsrc/HexSrc/old/W.BAT
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Making HEXEN.WAD
-echo Preprocessing: wadlink.txt to wadlink.wl
-wcc386 /dNOMAPS /dREGISTERED /p /fo=wadlink.wl \data\wadlink.txt
-echo wadlinking ...
-\tools\exe\wadlink
